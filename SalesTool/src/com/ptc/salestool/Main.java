6534388871d5da1c79d6ed2326f400d6ce44f240
Sita Raman Prasad Singh <ssingh@ptc.com>
2015-09-06 16:19:59 -0400

    Initial Checkin of sales Tool

---
+++ SalesTool/src/com/ptc/salestool/Main.java
@@ -0,0 +1,15 @@
+package com.ptc.salestool;
+
+public class Main {
+	public static void main(String[] args){
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hellow Happy Sales People!");
+		System.out.println("This App shows Sales Data");
+	}
+}
---
+++ SalesTool/src/com/ptc/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package com.ptc.salestool;
+
+public class SalesData {
+	int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("Sales Data needs to be implemented");
+	}
+}
